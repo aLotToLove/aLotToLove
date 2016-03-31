cadb309690733b589a2464d2d5fbce228e165b37
CJ Sampson <cjsampson41@gmail.com>
2016-03-31 18:24:15 -0500

    Base Template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/bootstrap.css">
+        <link rel="stylesheet" href="/css/cover.css">
+        <link rel="icon"type="image/png" href="/img/favicon.ico">
+        <title>A Lot To Love</title>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+		
+          <nav>
+            <ul class="nav masthead-nav">
+              <li class="active"><a href="Home">Home</a></li>
+              <li class="navbar_lists"><a href="howto">How To Guide</a></li>
+              <li class="navbar_lists"><a href="grants">Grants</a></li>
+              <li class="navbar_lists"><a href="inspiration">Inspiration</a></li>
+              <li class="navbar_lists"><a href="getfunding">Get Funding</a></li>
+              <li class="navbar_lists"><a href="contact">Contact</a></li>
+            </ul>
+          </nav>
+           
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    </body>
+</html>
