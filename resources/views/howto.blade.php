8aeee0f587b3c991a9ec7cb5229e31b6fb23c2bd
CJ Sampson <cjsampson41@gmail.com>
2016-04-11 17:18:12 -0500

    Added the text for the howto

--- resources/views/howto.blade.php
+++ resources/views/howto.blade.php
@@ -4,6 +4,43 @@
 
   @section('content')
 
-    <h1>How To Page!</h1>
+    <h1>How to Guide</h1>
+	<p>
+		Here's a few pointers for your lot to love project.  It's a simple toolkit designed to put you on the right track!
+	</p>
+	<div class="container">		
+		<section class="howToBlocks">
+			<h3>Sign a Maintenance Agreement</h3>
+		    <button class="btn btn-success">Agreement</button>
+		    <p>
+			    Many of the vacant parcels available are owned by the City of San Antonio.  These lots require an approved lease agreement.  Don't worry, it's easy to fill out and our friends at Keep San Antonio Beautiful are here to help you through the process.  We also want to keep our database up to date, so these agreements facilitate that process.
+		    </p>
+	    </section>
+
+	    <section class="howToBlocks">
+			<h3>Gather Volunteers</h3>
+		    <button class="btn btn-success">Volunteer</button>
+		    <p>
+			    Community support is the best way to guarantee the success of your project.  Neighbors, churches, and schools are all great resources.  The more help you have, the better!
+		    </p>
+	    </section>
+	    
+	    <section class="howToBlocks">
+			<h3>Sign a Maintenance Agreement</h3>
+		    <button class="btn btn-success">Pick A Design</button>
+		    <p>
+			    There are so many options available for your vacant lot beautification project.  If you peek at the design templates, there's great ideas to choose from.  There's also a list of native plants, which are great for our climate.
+		    </p>
+	    </section>
+	    <section class="howToBlocks">
+			<h3>Sign a Maintenance Agreement</h3>
+		    <button class="btn btn-success">Grant Research</button>
+		    <p>
+			    Many organizations in San Antonio and Texas have grant funding available for community based projects.  This funding can help buy materials for your project. Check our grants page for links to these awesome organizations.  Keep exploring on your own too, just incase we missed one!
+		    </p>
+	    </section>
+    
+    
+    
 
   @stop
