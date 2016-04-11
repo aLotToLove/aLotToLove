1a7a7a74f5883472b8e51a3eee7b45d529e1e352
CJ Sampson <cjsampson41@gmail.com>
2016-04-11 17:05:58 -0500

    worked on the navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,13 @@
 
   @section('content')
 
-    <h1>Welcome to A Lot to Love!</h1>
-
+	<h1 id="mainHeader">Welcome to A Lot to Love!</h1>
+	<div class="inner cover">
+		<img src="/img/purple-1.png" alt="icon" id="mainLogo">
+		<h1 class="cover-heading lot_starter">!A LOT to <span class= "lot_to_love">Love</span>!</h1>
+		<p class="lead">Help improve San Antonio by adobting one of these empty lots, It is not hard at all, just a few simple steps and you can be on your way to making your neighborhood a better place.</p>
+		<p class="lead">
+			<a href="#" class="btn btn-lg btn-default">Learn more</a>
+		</p>
+	</div>
   @stop
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,37 +2,73 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/bootstrap.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <link rel="stylesheet" href="/css/cover.css">
         <link rel="icon"type="image/png" href="/img/favicon.ico">
         <title>A Lot To Love</title>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </head>
-		
-    <nav>
-      <ul class="nav masthead-nav">
-        <li class="active"><a href="/">Home</a></li>
-        <li class="navbar_lists"><a href="howto">How To Guide</a></li>
-        <li class="navbar_lists"><a href="grants">Grants</a></li>
-        <li class="navbar_lists"><a href="inspiration">Inspiration</a></li>
-        <li class="navbar_lists"><a href="getfunding">Get Funding</a></li>
-        <li class="navbar_lists"><a href="contact">Contact</a></li>
-      </ul>
-    </nav>
 
+    <body class="testimg" background="/img/lot.jpg">
+         <nav>
+          <ul id="navigation">
+            <li class="active"><a href="/">Home</a></li>
+            <li class="navbar_lists"><a href="/howto">How To Guide</a></li>
+            <li class="navbar_lists"><a href="/grants">Grants</a></li>
+            <li class="navbar_lists"><a href="/inspiration">Inspiration</a></li>
+            <li class="navbar_lists"><a href="/getfunding">Get Funding</a></li>
+            <li class="navbar_lists"><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
 
-    <div class="container">
-      
-      @yield('content')
-      
+        @yield('content')
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p><a href="http://getbootstrap.com"></a>, <a href="https://twitter.com/mdo"></a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
 
-             
-  
+<!--modal for getting started navbar-->
+
+ <div class="modal fade" id="forGettingStartedModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Steps to adopt a lot.</h4>
+          </div>
+          <div class="modal-body modal-lists">
+            <ol>
+                <li class="modal-lists">Talk to your neighbors, organize a core group of volunteers.</li>
+                <br>
+                <li class"modal-lists">Fill out the maintenance agreement found on this webpage.</li>
+                <br>
+                <li class"modal-lists">How will you pay for it?  Check out the grant funding resources.</li>
+                <br>
+                <li class"modal-lists">Get inspiration for your design- which concept will you go for?  Check out our downloadable templates</li>
+                <br>
+                <li class"modal-lists">Get started!!!</li>
+            </ol>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+     <script src="/js/holder.min.js"></script>
     </body>
 </html>
